7eee5f5527d6b253b20d5988d842ec6dcf28cf97
Ibragim <mih230903@gmail.com>
2023-04-09 12:57:51 +0300

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-[#ff6c03] text-white p-3">
+    <div class="headContent mx-auto flex justify-between items-center">
+      <div class=" w-2/5 flex justify-between items-center">
+        <div>
+          <h1 class="text-3xl">Jig-Fishing</h1>
+        </div>
+        <div class="bg-[#1f1f1f] p-2 rounded-md w-96 flex justify-between items-center">
+          <input placeholder="Поиск по Jig-Fishing" class="text-white bg-[#1f1f1f] w-full mx-3" type="text" />
+          <span class="mdi mdi-magnify text-white"></span>
+        </div>
+      </div>
+      <div class="w-10 h-10 rounded-full overflow-hidden bg-[#1f1f1f]">
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.headContent {
+  width: 1500px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mx-auto my-9 bg-white">
+    <div id="content">
+      
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
